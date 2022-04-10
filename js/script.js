4beae6211fe73fddaaf10666a14032dda2fd1791
Ruslan <rnzamuraev@gmail.com>
2022-04-10 23:42:01 +0300

    add script(tabs)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
+window.addEventListener("DOMContentLoaded", function () {
+  "use strict";
 
+  const tabs = document.querySelectorAll(".info-header-tab"),
+    tabContents = document.querySelectorAll(".info-tabcontent"),
+    infoHeader = document.querySelector(".info-header");
+
+  function hideTabContent(a) {
+    for (let i = a; i < tabContents.length; i++) {
+      tabContents[i].classList.remove("show");
+      tabContents[i].classList.add("hide");
+    }
+  }
+  hideTabContent(1);
+
+  function showTabContent(b) {
+    if (tabContents[b].classList.contains("hide")) {
+      tabContents[b].classList.remove("hide");
+      tabContents[b].classList.add("show");
+    }
+  }
+
+  infoHeader.addEventListener("click", function (event) {
+    if (event.target && event.target.classList.contains("info-header-tab")) {
+      for (let i = 0; i < tabs.length; i++) {
+        if (event.target == tabs[i]) {
+          hideTabContent(0);
+          showTabContent(i);
+          break;
+        }
+      }
+    }
+  });
+});
